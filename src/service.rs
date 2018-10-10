6e7ca6f129ed13c66d0c2795d21a7ef196ff6851
Eelco Dolstra <edolstra@gmail.com>
2018-10-10 13:32:32 +0200

    Add support for serving utxo states
    For example,
    
      http://localhost:12080/mainnet/utxos/54
    
    gives a file containing the full utxo state at the end of epoch 54,
    while
    
      http://localhost:12080/mainnet/utxos-delta/53/54
    
    gives a file containing the delta between the end of epoch 53 and the
    end of epoch 54.

--- src/service.rs
+++ src/service.rs
@@ -22,6 +22,8 @@ fn start_http_server(cfg: &Config, networks: Arc<Networks>) -> iron::Listening {
     handlers::pack::Handler::new(networks.clone()).route(&mut router);
     handlers::epoch::Handler::new(networks.clone()).route(&mut router);
     handlers::tip::Handler::new(networks.clone()).route(&mut router);
+    handlers::utxos::Handler::new(networks.clone()).route(&mut router);
+    handlers::utxos_delta::Handler::new(networks.clone()).route(&mut router);
     info!("listening to port {}", cfg.port);
     iron::Iron::new(router)
         .http(format!("0.0.0.0:{}", cfg.port))
