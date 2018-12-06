fe3083b54e9f336fb52a7074b896ecb61d3639e4
rooooooooob <raftias@gmail.com>
2018-12-06 17:08:34 -0600

    Initial work on adding TxAux sending via the http-bridge.
    Only attempts to send to one peer right now.
    The send_transaction() part has not been tested yet, and is commented out for now.

--- src/handlers/common.rs
+++ src/handlers/common.rs
@@ -15,17 +15,22 @@ pub fn validate_epochid(v: &&str) -> Option<EpochId> {
     }
 }
 
-pub fn get_network_and_epoch<'a>(req: &Request, networks: &'a Networks) -> Option<(&'a Network, EpochId)> {
+pub fn get_network<'a>(req: &Request, networks : &'a Networks) -> Option<(String, &'a Network)> {
     let ref network_name = req.extensions.get::<Router>().unwrap().find("network").unwrap();
-    let ref epochid_str = req.extensions.get::<Router>().unwrap().find("epochid").unwrap();
 
     if ! validate_network_name (network_name) {
         return None;
     }
-    let net = match networks.get(network_name.to_owned()) {
-        None => return None,
-        Some(net) => net
-    };
+    
+    match networks.get(network_name.to_owned()) {
+        None => None,
+        Some(net) => Some((network_name.to_string(), net))
+    }
+}
+
+pub fn get_network_and_epoch<'a>(req: &Request, networks: &'a Networks) -> Option<(&'a Network, EpochId)> {
+    let (_, net) = get_network(req, networks)?;
+    let ref epochid_str = req.extensions.get::<Router>().unwrap().find("epochid").unwrap();
 
     let epochid = match validate_epochid (epochid_str) {
         None => {
